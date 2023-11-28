f524db256a296aa5c4a9100255434b717320df29
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2023-11-28 12:14:48 -0600

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {  RouterView } from 'vue-router';
+import MenuComponent from '@/components/common/MenuComponent.vue';
+
+
+</script>
+
+<template>
+   <div class="">
+        <MenuComponent />
+        <RouterView />
+   </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+//eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{html,js,vue, svg}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: "#1a3329",
+      }
+    },
+  },
+  plugins: [],
+}
+
